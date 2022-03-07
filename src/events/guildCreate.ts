0ac628a463c546e1cce5e595360dda3e8bf92de5
wert <me@mk3ext.dev>
2022-03-07 20:21:05 +0000

    Multi line string

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -4,7 +4,7 @@ import { createGuild } from '../utils/guild';
 import sendEmbed from '../utils/messages/sendEmbed';
 
 export default async function (client: Bot, guild: Guild) {
-    // Invited to New Guild
+    // Joined new guild
     client.logger.info(`Joined new guild ${guild.name} with ${guild.memberCount} members.`);
 
     const channel = (await guild.channels.fetch(guild.systemChannelId)) as TextChannel;
@@ -15,7 +15,13 @@ export default async function (client: Bot, guild: Guild) {
             channel,
             embed: {
                 title: `Hello ${guild.name}!`,
-                description: `My name is VVarden!\nYou can call me Warden or 5 Warden (V Warden).\n\nThank you for inviting me to your Discord Server!\nI'm trying to make the CFX Community a better place.\n\nMake sure to check my configuration settings by using \`/config\` command!\nI also need to have the permissions to kick and ban members, with a role higher than them!\n\nI'm already acting on new member joins.
+                description: `My name is VVarden!
+                You can call me Warden or 5 Warden (V Warden).
+                \nThank you for inviting me to your Discord Server!
+                I'm trying to make the CFX Community a better place.
+                \nMake sure to check my configuration settings by using \`/config\` command!
+                I also need to have the permissions to kick and ban members, with a role higher than them!
+                \nI'm already acting on new member joins.
                 \nIf you want to contribute to the project, use the Official Discord: <https://discord.gg/jeFeDRasfs>`,
                 color: 0x008000,
                 footer: {
