1075fdf7189664268ede03dc780aaacb64509e87
wert <me@mk3ext.dev>
2022-03-10 16:15:14 +0000

    tweak(ready): Register commands in guilds in parallel

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,9 +3,17 @@ import { Bot } from '../classes/Bot';
 export default async function (client: Bot) {
     client.logger.info('Bot is ready!');
 
-    client.guilds.cache.forEach(async guild => {
-        await guild.commands.fetch();
-        const arr = client.commands.map(c => c);
-        guild?.commands.set(arr);
-    });
+    await Promise.all(
+        client.guilds.cache.map(async guild => {
+            await guild.commands.fetch();
+            const arr = client.commands.map(c => c);
+            guild?.commands.set(arr);
+        })
+    );
+
+    // client.guilds.cache.forEach(async guild => {
+    //     await guild.commands.fetch();
+    //     const arr = client.commands.map(c => c);
+    //     guild?.commands.set(arr);
+    // });
 }
