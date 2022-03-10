6c3108eb318897a043173264e68b8e98abe648d1
wert <me@mk3ext.dev>
2022-03-10 13:15:25 +0000

    feat(checkserver): Added /checkserver <id>

---
+++ src/commands/user/checkserver.ts
@@ -0,0 +1,74 @@
+import { BaseCommandInteraction, Snowflake } from 'discord.js';
+import { Colours } from '../../@types';
+import { Bot, SlashCommand } from '../../classes';
+import { getBadServersByIDs } from '../../utils/badservers';
+import { sendEmbed } from '../../utils/messages';
+
+export default class CheckServerCommand extends SlashCommand {
+    constructor(client: Bot) {
+        super({
+            client,
+            name: 'checkserver',
+            description: 'Check if a server is bad',
+            type: 'CHAT_INPUT',
+            options: [
+                {
+                    type: 3,
+                    name: 'id',
+                    description: 'Server ID to check',
+                    required: true,
+                },
+            ],
+            defaultPermission: true,
+        });
+    }
+
+    public async run(client: Bot, interaction: BaseCommandInteraction): Promise<boolean> {
+        const sid = interaction.options.get('id')?.value as Snowflake;
+
+        if (sid.length !== 18) {
+            await sendEmbed({
+                interaction,
+                embed: {
+                    description: 'Invalid server id provided',
+                    color: Colours.YELLOW,
+                },
+            });
+            return false;
+        }
+
+        await getBadServersByIDs({ client, ids: [sid] })
+            .then(server => {
+                sendEmbed({
+                    interaction,
+                    embed: {
+                        title: ':shield: Server Blacklisted',
+                        color: Colours.RED,
+                        fields: [
+                            {
+                                name: 'Server Information',
+                                value: `**ID**: ${server[0].id} / **Name**: ${server[0].name}\n
+                                **Details**: ${server[0].type}\n
+                                **Date Added**:${server[0].createdAt
+                                    .toISOString()
+                                    .replace(/T/, ' ')
+                                    .replace(/\..+/, '')}\n
+                                **Added By**: ${server[0].addedBy}`,
+                            },
+                        ],
+                    },
+                });
+            })
+            .catch(() => {
+                sendEmbed({
+                    interaction,
+                    embed: {
+                        description: ':white_check_mark: Server ID not found in Database.',
+                        color: Colours.YELLOW,
+                    },
+                });
+            });
+
+        return true;
+    }
+}
