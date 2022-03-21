4b13b5ff63aafd10bf14644cfcb56047ef6a0d42
wert <me@mk3ext.dev>
2022-03-21 13:08:30 +0000

    global application commands

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,11 +3,5 @@ import { Bot } from '../classes/Bot';
 export default async function (client: Bot) {
     client.logger.info('Bot is ready!');
 
-    await Promise.all(
-        client.guilds.cache.map(async guild => {
-            await guild.commands.fetch();
-            const arr = client.commands.map(c => c);
-            guild?.commands.set(arr);
-        })
-    );
+    await client.application.commands.set(client.commands.map(c => c));
 }
