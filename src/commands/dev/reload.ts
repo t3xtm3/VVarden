5acc90d1302a7756bdf9e4115d06be1259b21935
wert <me@mk3ext.dev>
2022-03-21 14:50:01 +0000

    Reload ability

---
+++ src/commands/dev/reload.ts
@@ -0,0 +1,28 @@
+import { BaseCommandInteraction, TextBasedChannel } from 'discord.js';
+import { Bot, SlashCommand } from '../../classes';
+import { getProcessState, processInformationMsg } from '../../utils/helpers';
+
+export default class ProcfileCommand extends SlashCommand {
+    constructor(client: Bot) {
+        super({
+            client,
+            name: 'reload',
+            description: 'Reload the bot',
+            type: 'CHAT_INPUT',
+            options: [],
+            defaultPermission: true,
+            staffRole: 'dev',
+        });
+    }
+
+    public async run(client: Bot, interaction: BaseCommandInteraction): Promise<boolean> {
+        if (getProcessState() === 1) {
+            processInformationMsg(interaction);
+            return false;
+        }
+
+        client.commands.sweep(() => true);
+        await client.loadCommands(__dirname + '/commands');
+        return true;
+    }
+}
