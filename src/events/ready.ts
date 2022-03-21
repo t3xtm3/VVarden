deadbb659017019241f3db04683f4a891d7062f1
wert <me@mk3ext.dev>
2022-03-21 13:10:28 +0000

    global application commands

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,5 +3,9 @@ import { Bot } from '../classes/Bot';
 export default async function (client: Bot) {
     client.logger.info('Bot is ready!');
 
-    await client.application.commands.set(client.commands.map(c => c));
+    const arr = client.commands.map(c => c);
+    client.application.commands
+        .set(arr)
+        .then(() => client.logger.info('Loaded successfully'))
+        .catch(e => console.log(e));
 }
