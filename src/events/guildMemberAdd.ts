1329768785f5bce81722bf0f58cdddba0ada5525
wert <me@mk3ext.dev>
2022-03-29 17:31:27 +0100

    fix(guildMemberAdd): act like any other guild for main

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,16 +1,16 @@
-import { UserStatus, UserType } from '@prisma/client';
+import { UserStatus } from '@prisma/client';
 import { GuildMember } from 'discord.js';
 import { Bot } from '../classes/Bot';
 import { getGuild } from '../utils/guild';
 import { getUser } from '../utils/users';
 import { punishUser } from '../utils/users/punishUser';
-import data from '../config.json';
 
 export default async function (client: Bot, member: GuildMember) {
     const guild = member.guild;
     const settings = await getGuild({ client, id: guild.id });
 
     if (!settings) {
+        client.logger.error(`guildMemberAdd ${guild.name}: Unknown guild - Owner is: ${guild.ownerId}`);
         client.logger.error(
             `CRITICAL: GUILD HAS NO SETTINGS ${guild.name} ${guild.id} - OWNER: ${member.guild.ownerId}`
         );
@@ -21,13 +21,6 @@ export default async function (client: Bot, member: GuildMember) {
     if (!user) {
         return false;
     }
-
-    if (
-        guild.id === data.MAIN_GUILD &&
-        user.user_type !== UserType.SUPPORTER &&
-        user.user_type !== UserType.OWNER
-    )
-        return false;
 
     // Dynamically set this in future?
     if (user.status === UserStatus.BLACKLIST || user.status === UserStatus.PERM_BLACKLIST) {
