4e2f437b14fd40a953f426d44852867432ffddf4
wert <me@mk3ext.dev>
2022-04-03 14:50:01 +0100

    chore(ready): delete old command(s)

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -8,4 +8,14 @@ export default async function (client: Bot) {
         .set(arr)
         .then(() => client.logger.info('Loaded application commands successfully'))
         .catch(e => console.log(e));
+
+    client.application.commands.fetch().then(async commands => {
+        await commands.reduce(async (a, command) => {
+            await a;
+            if (command.name === 'badserver') {
+                client.application.commands.delete(command.id);
+                client.logger.info(`Deleting command ${command.name} - ${command.id}`);
+            }
+        }, Promise.resolve());
+    });
 }
